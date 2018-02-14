96fabdf300c58207e9825167780826b0587b3d64
mbach7 <mbach@igb.illinois.edu>
2018-02-14 15:55:56 -0600

    updated .gitignore

---
+++ conf/settings.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+
+define("mysql_host", "localhost");
+define("mysql_user", "tapelibraryuser");
+define("mysql_password", "yCx7wZUmr:6,9sVD");
+define("mysql_database", "tapelibrary");
+
+define("__TITLE__", "Backup Tape Library");
+
+define("__LDAP_HOST__","auth.igb.uiuc.edu");
+define("__LDAP_BASE_DN__","dc=igb,dc=uiuc,dc=edu");
+define("__LDAP_PEOPLE_OU__","ou=people,dc=igb,dc=uiuc,dc=edu");
+define("__LDAP_GROUP_OU__","ou=group,dc=igb,dc=uiuc,dc=edu");
+define("__LDAP_COMPUTER_OU__","ou=computer,dc=igb,dc=uiuc,dc=edu");
+define("__LDAP_HOST_OU__","ou=host,dc=igb,dc=uiuc,dc=edu");
+define("__LDAP_ADMIN_GROUP__","cn=ldap_web,ou=group,dc=igb,dc=uiuc,dc=edu");
+define("__LDAP_SSL__",FALSE);
+define("__LDAP_PORT__",389);
+define("__SESSION_TIMEOUT__",36000);
+define("__SESSION_NAME__","tapelibrarysession");
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
