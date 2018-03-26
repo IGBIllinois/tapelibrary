78c38400b9714fd5ad42d92419d735715c2ed87a
mbach7 <mbach@igb.illinois.edu>
2018-03-26 18:09:59 -0500

    Removed debug statements

--- html/includes/PHPExcel-1.8/Classes/PHPExcel/Writer/Excel2007.php
+++ html/includes/PHPExcel-1.8/Classes/PHPExcel/Writer/Excel2007.php
@@ -187,9 +187,7 @@ class PHPExcel_Writer_Excel2007 extends PHPExcel_Writer_Abstract implements PHPE
             // If $pFilename is php://output or php://stdout, make it a temporary file...
             $originalFilename = $pFilename;
             if (strtolower($pFilename) == 'php://output' || strtolower($pFilename) == 'php://stdout') {
-                echo("tmpdir = ".PHPExcel_Shared_File::sys_get_temp_dir()."<BR>");
                 $pFilename = @tempnam(PHPExcel_Shared_File::sys_get_temp_dir(), 'phpxltmp');
-                echo("tempfilename = $pFilename<BR>");
                 if ($pFilename == '') {
                     $pFilename = $originalFilename;
                 }
