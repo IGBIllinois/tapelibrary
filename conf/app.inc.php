6d3399fdacf6fac4da78904fd7a5a8a1799c778c
David Slater <dslater@dslater-lnx.igb.illinois.edu>
2024-07-31 12:49:58 -0500

    Added github workflows Added IGBIllinois-php dependency

---
+++ conf/app.inc.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Stores application variables
+*/
+define("VERSION","2.0");
+define("WEBSITE_URL","https://github.com/IGBIllinois/tapelibrary");
+
+
+
+?>
--- html/includes/main.inc.php
+++ html/includes/main.inc.php
@@ -1,26 +1,32 @@
 <?php
 
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-set_include_path(get_include_path().";../libs;../conf;");
+$include_paths = array(__DIR__ . '/../../libs');
+set_include_path(get_include_path() . ":" implode(':',$include_paths));
 
+require_once __DIR__ . '/../../conf/app.inc.php';
+require_once __DIR__ . '/../../conf/settings.inc.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 function my_autoloader($class_name) {
-	if(file_exists("../libs/" . $class_name . ".class.inc.php")) {
-
-		require_once "../libs/" .$class_name . '.class.inc.php';
+	if(file_exists(__DIR__ . "/../../libs/" . $class_name . ".class.inc.php")) {
+		require_once $class_name . '.class.inc.php';
 	}
-
 }
 spl_autoload_register('my_autoloader');
 
-include_once('../conf/settings.inc.php');
+$ldap = new \IGBIllinois\ldap(__LDAP_HOST__,
+                        __LDAP_BASE_DN__,
+                        __LDAP_PORT__,
+                       __LDAP_SSL__);
 
-require_once '../vendor/autoload.php';
-
-
-$ldap = new ldap(__LDAP_HOST__,__LDAP_SSL__,__LDAP_PORT__,__LDAP_BASE_DN__);
-$db = new db(mysql_host,mysql_database,mysql_user,mysql_password);
+$db = new \IGBIllinois\db(mysql_host,
+			mysql_database,
+			mysql_user,
+			mysql_password);
 
 // These lines allow a user to hit the Back button and return to a previously
 // submitted form
